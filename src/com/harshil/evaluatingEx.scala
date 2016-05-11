6213592bf5e3d8ea8139b83e3c523e4bee35c86a
Harshil Chokshi <harshilc82@gmail.com>
2016-05-11 12:43:58 +0530

    Evaluating e^x expression solution

--- src/com/harshil/evaluatingEx.scala
+++ src/com/harshil/evaluatingEx.scala
@@ -1,16 +1,26 @@
 package com.harshil
 
-import java.math.BigDecimal
+import java.lang.Float
 
 
 object evaluatingEx {
    
   def main (args:Array[String]){
-    println(f(20.0000f));
+    println(f(20.0000f))
   }
   
-  def f(x: Float){
-    var a = Math.pow(Math.E, x)
-    
+  def f(x:Float):Double={
+   BigDecimal(createExp(x)).setScale(4, BigDecimal.RoundingMode.FLOOR).toDouble
   }
+  
+  def createExp(x:Float):Double={
+    var ex:Double = 0;
+    for(i<- 0 to 9)  ex = ex + (Math.pow(x.doubleValue(), i)/fact(i)) 
+    ex
+  }
+  
+  def fact(x:Double):Double={
+    if(x==0) 1 else x*fact(x-1)
+  }
+
 }
